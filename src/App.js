015822da9efc8336e7fcf2c21e47d6935f0826e6
jkb <skjin20800@naver.com>
2023-02-10 14:07:02 +0900

    simpleDiary

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import DiaryEditor from './DiaryEdit';
+import DiaryList from './DiaryList';
+
+const dummyList = [
+  {
+    id: 1,
+    author: '진경빈',
+    content: '하이 2',
+    emotion: 5,
+    created_date: new Date().getTime(), // 현재시간 기준 생성
+  },
+  {
+    id: 2,
+    author: '홍길동',
+    content: '하이 2',
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: '김용관',
+    content: '하이 3',
+    emotion: 1,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 4,
+    author: '이영균',
+    content: '하이 4',
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <DiaryEditor />
+        <DiaryList diaryList={dummyList} />
       </header>
     </div>
   );
---
+++ src/DiaryEdit.js
@@ -0,0 +1,67 @@
+import { useRef, useState } from 'react';
+
+const DiaryEditor = () => {
+  const authorInput = useRef();
+  const contentInput = useRef();
+
+  //하나의 state 객체안에 author와 content 둘다 관리함
+  const [state, setState] = useState({
+    author: '',
+    content: '',
+    emtion: 1,
+  });
+
+  const handleChangeState = e => {
+    setState({
+      ...state, //기존의 값은 원본을 유지함 state의 값을 넣음 [값 변경 x]
+      [e.target.name]: e.target.value, //새로운 값만 변경 [값 변경 O]
+    });
+  };
+
+  const handleSumbit = () => {
+    console.log(state);
+    if (state.author.length < 1) {
+      alert('작성자는 최소 1글자 이상 입력해주세요');
+      authorInput.current.focus();
+      return;
+    }
+    if (state.content.length < 5) {
+      alert('일기 본문은 최소 5글자 이상 입력해주세요');
+      contentInput.current.focus();
+      return;
+    }
+
+    alert('저장 성공');
+  };
+
+  return (
+    <div className="DiaryEditor">
+      <h2>오늘의 일기</h2>
+      <div>
+        <input
+          ref={authorInput}
+          name="author"
+          value={state.author}
+          //input 값이 변경되었을때 실행되는 메서드
+          onChange={handleChangeState}
+        />
+      </div>
+      <div>
+        <textarea name="content" value={state.content} ref={contentInput} onChange={handleChangeState} />
+      </div>
+      <div>
+        <select name="emotion" value={state.emtion} onChange={handleChangeState}>
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </div>
+      <div>
+        <button onClick={handleSumbit}>일기 저장하기</button>
+      </div>
+    </div>
+  );
+};
+export default DiaryEditor;
