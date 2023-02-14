22e160910bdf2fa94b01b56a2275c87b25783bc2
jkb <skjin20800@naver.com>
2023-02-14 19:11:39 +0900

    React Lifecycle - useEffect

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState, useRef } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEdit';
 import DiaryList from './DiaryList';
+import Lifecycle from './Lifecycle';
 
 function App() {
   const [data, setData] = useState([]);
@@ -32,10 +33,9 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <DiaryEditor onCreate={onCreate} />
-        <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
-      </header>
+      <Lifecycle />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
   );
 }
