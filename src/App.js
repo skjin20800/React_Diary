99de4bbd946a55b0dc30a55d131fcbd1f12e02f4
JinKyoungBin <skjin20800@naver.com>
2023-02-14 21:40:38 +0900

    Call Api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEdit';
 import DiaryList from './DiaryList';
-import Lifecycle from './Lifecycle';
+
+//https://jsonplaceholder.typicode.com/comments //json xxxxxfdfdxpxxxasdfasdfasdfasdf
 
 function App() {
   const [data, setData] = useState([]);
   const dataId = useRef(0);
+  const getData = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/comments').then(res => res.json());
+    const initData = res.slice(0, 20).map(it => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
     const newItem = {
@@ -33,7 +52,6 @@ function App() {
 
   return (
     <div className="App">
-      <Lifecycle />
       <DiaryEditor onCreate={onCreate} />
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
